fcfdaf79cdc339e5b3b6b58ac139a6dd793ad7e1
Joanmei <joanburgei7@gmail>
2022-09-13 02:28:09 -0700

    _putchar printed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
- *  * main - prints the string "_putchar" from a character array.
+ * main - prints the string "_putchar" from a character array.
  *
- *   * Return: 0 on success
- *    */
+ * Return: 0 on success
+ */
 int main(void)
 {
 char c[] = "_putchar";
 int i;
 for (i = 0; i < 9; i++)
 {
-_putchar(c[i]);
+putchar(c[i]);
 }
-_putchar('\n');
+putchar('\n');
 return (0);
 }
